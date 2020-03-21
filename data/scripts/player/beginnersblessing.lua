df113e7c150d20ae2ef9916861738721c7c3d4a2
Rinart73 <rinart73@gmail.com>
2020-03-21 04:22:29 +0100

    0.3 * Updated to 0.31.1 - 1.0.x

--- data/scripts/player/beginnersblessing.lua
+++ data/scripts/player/beginnersblessing.lua
@@ -1,7 +1,7 @@
 if onClient() then return end -- server side
 
 package.path = package.path .. ";data/scripts/lib/?.lua"
-local BuffsH = include("BuffsHelper")
+local Buffs = include("BuffsHelper")
 
 -- namespace BeginnersBlessing
 BeginnersBlessing = {}
@@ -14,16 +14,16 @@ function BeginnersBlessing.initialize()
     if shipsCreated == 3 then terminate() end -- there is no need in this script anymore
 
     player:registerCallback("onSectorEntered", "onSectorEntered")
-    Sector():registerCallback("onEntityCreate", "onEntityCreate") -- we need to catch when player creates a ship
+    Sector():registerCallback("onEntityCreated", "onEntityCreated") -- we need to catch when player creates a ship
 end
 
 function BeginnersBlessing.onSectorEntered(playerIndex)
-    if Player().index ~= playerIndex then return end
-
-    Sector():registerCallback("onEntityCreate", "onEntityCreate")
+    if Player().index == playerIndex then
+        Sector():registerCallback("onEntityCreated", "onEntityCreated")
+    end
 end
 
-function BeginnersBlessing.onEntityCreate(entityId)
+function BeginnersBlessing.onEntityCreated(entityId)
     local entity = Entity(entityId)
     local player = Player()
     if entity.isShip and entity.factionIndex == player.index then
@@ -31,21 +31,20 @@ function BeginnersBlessing.onEntityCreate(entityId)
             shipsCreated = shipsCreated + 1
             player:setValue("BeginnersBlessing", shipsCreated)
 
-            -- Since we're applying buff right after the entity was created (before game calls 'restore') we will not be able to get 'addBuff' return values
-            -- Adding buffs and bonuses is supposed to be done only with 'BuffsHelper' or via `invokeFunction`! Otherwise game will cancel bonuses after this script will be terminated.
-            BuffsH.addBuff(
-              entity,
-              "Beginner's Blessing", -- buff name without translation (%_t)
+            -- Since we're applying buff right after the entity was created (before game calls 'restore') there is a good chance that we will not be able to get 'addBuff' return values
+            -- Adding buffs and bonuses is supposed to be done ONLY via 'BuffsHelper.lua' and NOT directly! Otherwise game will cancel bonuses after this script will be terminated.
+            Buffs.addBuff(entity,
+              "Beginner's Blessing"%_T,
               { -- bonuses, for more info look at game 'Entity' Docs: addBaseMultiplier, addMultiplier, addMultiplyableBias, addAbsoluteBias
-                { type = BuffsH.Type.BaseMultiplier, stat = StatsBonuses.RadarReach, value = 0.2 }, -- +20%
-                { type = BuffsH.Type.BaseMultiplier, stat = StatsBonuses.ScannerReach, value = 0.25 }, -- +25%
-                { type = BuffsH.Type.BaseMultiplier, stat = StatsBonuses.ScannerMaterialReach, value = 0.25 }, -- +25%
-                { type = BuffsH.Type.MultiplyableBias, stat = StatsBonuses.HyperspaceReach, value = 1 }, -- +1
-                { type = BuffsH.Type.BaseMultiplier, stat = StatsBonuses.HyperspaceCooldown, value = -0.2 }, -- -20%
-                BuffsH.Custom.ChangeDurability:New(20, 5) -- custom effect, for more info look at 'BuffsHelper.lua' in the 'Buffs' mod
+                { Buffs.Type.BaseMultiplier, StatsBonuses.RadarReach, 0.2 }, -- +20%
+                { Buffs.Type.BaseMultiplier, StatsBonuses.ScannerReach, 0.25 }, -- +25%
+                { Buffs.Type.BaseMultiplier, StatsBonuses.ScannerMaterialReach, 0.25 }, -- +25%
+                { Buffs.Type.MultiplyableBias, StatsBonuses.HyperspaceReach, 1 }, -- +1 but can be multiplied by some other buff with Buffs.Type.Multiplier
+                { Buffs.Type.BaseMultiplier, StatsBonuses.HyperspaceCooldown, -0.2 }, -- -20%
+                Buffs.Scripts.ChangeDurability:New(20, 5) -- custom script, for more info look at 'BuffsHelper.lua' or at the Buffs Docs
               },
               10 * 60, -- 10 minutes
-              BuffsH.ApplyMode.Add,
+              Buffs.Mode.Add,
               false, -- buff doesn't decay when sector is unloaded
               "BeginnersBlessing", -- icon name
               nil, -- don't need to specify description, because we already did in the 'BuffsIntegration.lua'
